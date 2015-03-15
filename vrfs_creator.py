fb4794c8794ee864cbfe04cc93cab6ce8568ec26
SynMan <RubenFilipe@users.noreply.github.com>
2015-03-15 03:50:45 +0000

    Update vrfs_creator.py

--- vrfs_creator.py
+++ vrfs_creator.py
@@ -1,40 +1,22 @@
+
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+
+from settings import *
 
 print("""
 
 	Gerador VRF RIS-DADOS v0.1
 
 	""")
-################# Info Base ##################
-desc={
-	"geral" : "-RISD",
-	"pri" : "-P",
-	"back" : "-B",
-	"con" : "-C",
-	"as" : "2860:",
-	"palha":"0000"
-}
 
-vrfs={
-	"1":"VPN_RIS",
-	"2":"VPN_RIS_WIFI_UTENTES",
-	"3":"VPN_SICAD",
-	"4":"VPN_CH_CHLO",
-	"5":"VPN_CH_CHTV",
-	"6":"VPN_CH_CHBV"
-	}
 
 ###Options VRF
 def fun_opt():	
-	site_id=raw_input("Inserir Site ID : ")
-	vrf_id = raw_input("Nr VRF : ")
-	print("""
-	1 - Principal
-	2 - Backup
-	3 - Contigência
-		""")
-	tipo= raw_input("Escolhe tipo site : ")
+	site_id=raw_input(text["sid"])
+	vrf_id = raw_input(text["vnr"])
+	print(text["menu2"])
+	tipo= raw_input(text["tps"])
 	if tipo == '1':
 		tipo="P"
 	elif tipo == '2':
@@ -42,9 +24,9 @@ def fun_opt():
 	elif tipo == '3':
 		tipo="C"
 	else:
-		print("Escolhe tipo de site correcto!")
+		print(vrf_texts["site_text"])
 	tipo_site = "-" + tipo
-	mse=raw_input("Inserir Nr MSE:")
+	mse=raw_input(text["c_mse"])
 	vrf,code = select_vrf()
 	print_vrf_opt(site_id,vrf_id,tipo_site,mse,vrf,code)
 ##Print VRF
@@ -56,14 +38,10 @@ def print_vrf_opt(site_id,vrf_id,tipo_site,mse,vrf,code):
 	print("	" + desc["as"] + code + desc["palha"] + " (" + vrf + desc["geral"] + site_id + tipo_site + ", RTi)")
 ##Print VRF
 def fun_zon():	
-	site_id=raw_input("Inserir Site ID : ")
-	vrf_id = raw_input("Nr VRF : ")
-	print("""
-	1 - Principal
-	2 - Backup
-	3 - Contigência
-		""")
-	tipo= raw_input("Escolhe tipo site : ")
+	site_id=raw_input(text["sid"])
+	vrf_id = raw_input(text["vnr"])
+	print(text["menu2"])
+	tipo= raw_input(text["tps"])
 	if tipo == '1':
 		tipo="P"
 	elif tipo == '2':
@@ -71,12 +49,12 @@ def fun_zon():
 	elif tipo == '3':
 		tipo="C"
 	else:
-		print("Escolhe tipo de site correcto!")
+		print(vrf_texts["site_text"])
 	tipo_site = "-" + tipo
-	erip_one=raw_input("ERIP P:")
-	erip_one_id=raw_input("Erip Id : ")
-	erip_two=raw_input("ERIP B:")
-	erip_two_id=raw_input("Erip2 Id: ")
+	erip_one=raw_input(erips_text["ep"])
+	erip_one_id=raw_input(erips_text["epid"])
+	erip_two=raw_input(erips_text["ep2"])
+	erip_two_id=raw_input(erips_text["ep2id"])
 	vrf,code = select_vrf()
 	print_vrf_zon(site_id,vrf_id,tipo_site,vrf,code,erip_one_id,erip_two_id,erip_one,erip_two)
 ##Print VRF
@@ -89,14 +67,10 @@ def print_vrf_zon(site_id,vrf_id,tipo_site,vrf,code,erip_one_id,erip_two_id,erip
 	print("	" + desc["as"] + code + desc["palha"] + " (" + vrf + desc["geral"] + site_id + tipo_site + ", RTi)")
 ## VRF Options
 def fun_nos():	
-	site_id=raw_input("Inserir Site ID : ")
-	vrf_id=raw_input("Nr VRF : ")
-	print("""
-	1 - Principal
-	2 - Backup
-	3 - Contigência
-		""")
-	tipo= raw_input("Escolhe tipo site : ")
+	site_id=raw_input(text["sid"])
+	vrf_id=raw_input(text["vnr"])
+	print(text["menu2"])
+	tipo= raw_input(text["tps"])
 	if tipo == '1':
 		tipo="P"
 	elif tipo == '2':
@@ -104,10 +78,10 @@ def fun_nos():
 	elif tipo == '3':
 		tipo="C"
 	else:
-		print("Escolhe tipo de site correcto!")
+		print(vrf_texts["site_text"])
 	tipo_site = "-" + tipo
-	erip_one=raw_input("ERIP P:")
-	erip_one_id=raw_input("Erip Id : ")
+	erip_one=raw_input(erips_text["ep"])
+	erip_one_id=raw_input(erips_text["epid"])
 	vrf,code = select_vrf()
 	print_vrf_nos(site_id,vrf_id,tipo_site,vrf,code,erip_one_id,erip_one)
 ##Print VRF
@@ -119,48 +93,43 @@ def print_vrf_nos(site_id,vrf_id,tipo_site,vrf,code,erip_one_id,erip_one):
 	print("	" + desc["as"] + code + desc["palha"] + " (" + vrf + desc["geral"] + site_id + tipo_site + ", RTi)")
 ############# FIM NGN ###################
 def select_vrf():
-	print("Escolher VRF adequada ao Circuito!\n")
+	print(vrf_texts["cir_text"])
 	for linha in vrfs:
 		for num in linha:
 			print(num + " - " + vrfs[linha])
-	vrf=raw_input("Escolhe VRF --> ")
+	vrf=raw_input(text["c_vrf"])
 	if vrf == '1' :
 		vrf=vrfs['1']
-		code="11268"
+		code=vrfs_rds["VPN_RIS"]
 	elif vrf == '2' :
 		vrf=vrfs['2']
-		code="11422"
+		code=vrfs_rds["VPN_RIS_WIFI_UTENTES"]
 	elif vrf == '3' :
 		vrf=vrfs['3']
-		code=""
+		code=vrfs_rds["VPN_SICAD"]
 	elif vrf == '4' :
 		vrf=vrfs['4']
-		code=""
+		code=vrfs_rds["VPN_CH_CHLO"]
 	elif vrf == '5' :
 		vrf=vrfs['5']
-		code=""
+		code=vrfs_rds["VPN_CH_CHTV"]
 	elif vrf == '6' :
 		vrf=vrfs['6']
-		code=""
+		code=vrfs_rds["VPN_CH_CHBV"]
 	else :
-		print("Escolhe uma VRF da lista.")
+		print(vrf_texts["list_text"])
 		return select_vrf()
 	return vrf,code
 ##Main function
 def fun_main():
-	print("""
-Escolher Operador
-
-1- Optimus
-2- ZON
-3- NÓS NGN
-	""")
-	opc=raw_input("ISP-->")
+	print(text["menu1"])
+	opc=raw_input(text["isp"])
 	if opc == '1':
 		fun_opt()
 	elif opc == '2':
 		fun_zon()
 	elif opc == '3':
 		fun_nos()
+
 ##Run Main Function
 fun_main()
