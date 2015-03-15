b2eec2ad4a4b1b2d8d217377aff941baa944b2d7
SynMan <RubenFilipe@users.noreply.github.com>
2015-03-15 03:51:41 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+## Version 0.2
+desc={
+	"geral" : "-RISD",
+	"pri" : "-P",
+	"back" : "-B",
+	"con" : "-C",
+	"as" : "2860:",
+	"palha":"0000"
+}
+
+vrfs={
+	"1":"VPN_RIS",
+	"2":"VPN_RIS_WIFI_UTENTES",
+	"3":"VPN_SICAD",
+	"4":"VPN_CH_CHLO",
+	"5":"VPN_CH_CHTV",
+	"6":"VPN_CH_CHBV"
+	}
+
+vrfs_rds={
+	"VPN_RIS":"11268",
+	"VPN_RIS_WIFI_UTENTES":"11422",
+	"VPN_SICAD":"xxxxx",
+	"VPN_CH_CHLO":"xxxxx",
+	"VPN_CH_CHTV":"xxxxx",
+	"VPN_CH_CHBV":"xxxxx"
+	}
+
+
+text={
+	"isp" : "ISP --> ",
+	"sid" : "Site ID --> ",
+	"vnr" : "Nr VRF --> ",
+	"tps" : "Tipo de Site --> ",	
+	"c_mse" : "MSE --> ",
+	"menu2" : "\n 1 - Principal \n 2 - Backup \n 3 - Contigência \n",
+	"menu1" : "\n 1 - Optimus \n 2 - ZON \n 3 - NÓS NGN \n",
+	"c_vrf" : "Escolhe VRF --> ",
+
+}
+
+
+vrf_texts={
+	"list_text" : "Escolhe uma VRF da lista.",
+	"cir_text" : "Escolher VRF adequada ao Circuito!",
+	"site_text" : "Escolhe tipo de site correcto!\n",
+
+
+}
+
+erips_text={
+	"ep" : "ERIP P : ",
+	"ep1id" : "Erip Id : ",
+	"ep2" : "ERIP B : ",
+	"ep2id" : "Erip2 Id : "
+
+}
+
+##### List PE's ID's
+pe_ids={
+
+"AFC1ERIP001":"0001",
+"AFC1ERIP002":"0002",
+"AGP1ERIP001":"0003",
+"AGP1ERIP002":"0004",
+"ALG1ERIP001":"0005",
+"ALG1ERIP002":"0006",
+"ALV1ERIP001":"0007",
+"ALV1ERIP002":"0008"
+}
+
+####Fim lista
